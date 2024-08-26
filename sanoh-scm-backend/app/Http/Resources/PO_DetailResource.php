6a9563b3bc5834180e2d3acd930d0c88b1fd28d9
RizqiFarezi <rizqifarezi@gmail.com>
2024-08-25 21:49:07 -0700

    feat: add price and po_no on podetail resource

--- sanoh-scm-backend/app/Http/Resources/PO_DetailResource.php
+++ sanoh-scm-backend/app/Http/Resources/PO_DetailResource.php
@@ -15,8 +15,10 @@ class PO_DetailResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'po_no' => $this->po_no,
             'bp_part_no' => $this->bp_part_no,
             'item_desc_a' => $this->item_desc_a,
+            'price' => $this->price,
             'purchase_unit' => $this->purchase_unit,
             'po_qty' => $this->po_qty,
         ];
