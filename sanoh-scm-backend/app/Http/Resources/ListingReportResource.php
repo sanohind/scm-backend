df4d3c181b9c73706d95fce83de508b01bff96b7
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-09-04 08:37:26 +0700

    fix(resource): fix ListingReportResource by adding chopStr function the respond of filepath value

--- sanoh-scm-backend/app/Http/Resources/ListingReportResource.php
+++ sanoh-scm-backend/app/Http/Resources/ListingReportResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,8 +19,16 @@ class ListingReportResource extends JsonResource
             'po_listing_no' => $this->po_listing_no,
             'bp_code' => $this->bp_code,
             'date' => $this->date,
-            'file' => $this->file,
+            'file' => $this->chopStr(),
             'upload_at' => $this->upload_at
         ];
+    }
+
+    // only return the filename
+    private function chopStr(){
+        $string = $this->file;
+        $chopFile = Str::chopStart($string, '/storage/listing_report/');
+
+        return $chopFile;
     }
 }
