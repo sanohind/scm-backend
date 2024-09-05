85e1c5caf012986420fbf9301db1cda2d2cce648
maqilrp <aqilrasyid2004@gmail.com>
2024-09-05 08:19:02 +0700

    fix: fix cors

--- config/cors.php
+++ config/cors.php
@@ -36,7 +36,7 @@ return [
     |
     */
 
-    'allowed_origins' => ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://sanoh-scm.vercel.app', 'https://api.edutrashgo.com','http://ssp.ns1.sanoh.co.id/','http://be-ssp.ns1.sanoh.co.id/'],
+    'allowed_origins' => ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://sanoh-scm.vercel.app', 'https://api.edutrashgo.com','http://ssp.ns1.sanoh.co.id','http://be-ssp.ns1.sanoh.co.id'],
 
     /*
     |--------------------------------------------------------------------------
