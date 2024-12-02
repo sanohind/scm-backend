22edd4626b9d48c7961f5a79129e50b7ebadd998
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-12-02 11:04:17 +0700

    fix: fix index po auth user

--- app/Http/Controllers/Api/HistoryController.php
+++ app/Http/Controllers/Api/HistoryController.php
@@ -16,7 +16,7 @@ class HistoryController
     public function poHeaderHistory(Request $request)
     {
         $check =Auth::user()->role;
-        if ($check == 5) {
+        if ($check == 5 || $check == 6) {
             $user = Auth::user()->bp_code;
         } elseif ($check == 2) {
             // dd($request);
