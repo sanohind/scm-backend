bf23263af19d2b27f3c1d5e2fa45b8be61da0069
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-12-19 09:09:21 +0700

    fix: add conditional receipt null in DN_HistoryViewResource v2

--- app/Http/Resources/DeliveryNote/DN_HistoryViewResource.php
+++ app/Http/Resources/DeliveryNote/DN_HistoryViewResource.php
@@ -43,8 +43,8 @@ class DN_HistoryViewResource extends JsonResource
         // Convert and format date and time to strings
         $dnDetail = $this->dnDetail->first();
 
-        $date = $dnDetail->actual_receipt_date;
-        $time = $dnDetail->actual_receipt_time;
+        $date = $dnDetail->actual_receipt_date ?? null;
+        $time = $dnDetail->actual_receipt_time ?? null;
 
         if ($date == null && $time == null || $date == null && $time != null || $date != null && $time == null)  {
             $concat = null;
