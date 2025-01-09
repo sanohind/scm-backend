b3907f52f5e3dadc8416eb06c5186e7ca21e5641
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-01-09 11:38:33 +0700

    fix: fix item_old_name doesnt get

--- app/Service/Subcontractor/SubcontGetListItem.php
+++ app/Service/Subcontractor/SubcontGetListItem.php
@@ -36,7 +36,7 @@ class SubcontGetListItem
         }
 
         // Get record of subcont item data
-        $data = SubcontItem::select('item_code','item_name')
+        $data = SubcontItem::select('item_code','item_name', 'item_old_name')
             ->where('bp_code', $user)
             ->where('status', '1')
             ->orderBy('item_name', 'asc')
