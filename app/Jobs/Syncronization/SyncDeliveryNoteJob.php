c2522204919c08cb29cfc42f4c8389dafaf670f2
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-04-14 08:03:45 +0700

    refactor: Change sync po and dn handle from using service class to raw code (cause handling job process while accessing service class take more time then raw code)

--- app/Jobs/Syncronization/SyncDeliveryNoteJob.php
+++ app/Jobs/Syncronization/SyncDeliveryNoteJob.php
@@ -4,12 +4,15 @@ namespace App\Jobs\Syncronization;
 
 use Carbon\Carbon;
 use App\Trait\ErrorLog;
+use App\Models\DeliveryNote\DnDetail;
+use App\Models\DeliveryNote\DnHeader;
+use App\Models\DeliveryNote\DnDetailErp;
+use App\Models\DeliveryNote\DnHeaderErp;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Models\PurchaseOrder\PoHeaderErp;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Service\Syncronization\SyncDeleteData;
-use App\Service\Syncronization\SyncDeliveryNoteData;
 
 class SyncDeliveryNoteJob implements ShouldQueue
 {
@@ -26,7 +29,7 @@ class SyncDeliveryNoteJob implements ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle(SyncDeliveryNoteData $syncDeliveryNoteData, SyncDeleteData $syncDeleteData): void
+    public function handle(SyncDeleteData $syncDeleteData): void
     {
         try {
             // Initialize year and period
@@ -48,7 +51,70 @@ class SyncDeliveryNoteJob implements ShouldQueue
                     ->pluck('po_no')->toArray();
             }
 
-            $syncDeliveryNoteData->syncDeliveryNote($poNumber);
+            // Delivery Note Header
+            $dnNumber = [];
+            foreach ($poNumber as $data) {
+                $sqlsrvDataDnHeader = DnHeaderErp::where('po_no', $data)->get();
+                // copy all data from sql server
+                foreach ($sqlsrvDataDnHeader as $data) {
+                    $dnNumber[] = $data->no_dn;
+
+                    DnHeader::updateOrCreate(
+                        [
+                            'no_dn' => $data->no_dn,
+                            'po_no' => $data->po_no,
+                        ],
+                        [
+                            'supplier_code' => $data->supplier_code,
+                            'supplier_name' => $data->supplier_name,
+                            'dn_created_date' => $data->dn_create_date,
+                            'dn_year' => $data->dn_year,
+                            'dn_period' => $data->dn_period,
+                            'plan_delivery_date' => $data->plan_delivery_date,
+                            'plan_delivery_time' => $data->plan_delivery_time,
+                            'status_desc' => $data->status_desc,
+                            'packing_slip' => $data->packing_slip,
+                        ]
+                    );
+                }
+            }
+
+            // Delivery Note Detail
+            foreach ($dnNumber as $data) {
+                $sqlsrvDataDnDetail = DnDetailErp::where('no_dn', $data)->get();
+                // copy all data from sql server
+                foreach ($sqlsrvDataDnDetail as $data) {
+                    DnDetail::updateOrCreate(
+                        [
+                            'no_dn' => $data->no_dn,
+                            'dn_line' => $data->dn_line,
+                        ],
+                        [
+                            'order_origin' => $data->order_origin,
+                            'plan_delivery_date' => $data->plan_delivery_date,
+                            'plan_delivery_time' => $data->plan_delivery_time,
+                            'actual_receipt_date' => $data->actual_receipt_date,
+                            'actual_receipt_time' => $data->actual_receipt_time,
+                            'no_order' => $data->no_order,
+                            'order_set' => $data->order_set,
+                            'order_line' => $data->order_line,
+                            'order_seq' => $data->order_seq,
+                            'part_no' => $data->part_no,
+                            'supplier_item_no' => $data->supplier_item_no,
+                            'item_desc_a' => $data->item_desc_a,
+                            'item_desc_b' => $data->item_desc_b,
+                            'lot_number' => $data->lot_number,
+                            'dn_qty' => $data->dn_qty,
+                            'receipt_qty' => $data->receipt_qty,
+                            'dn_unit' => $data->dn_unit,
+                            'dn_snp' => $data->dn_snp,
+                            'reference' => $data->reference,
+                            'status_desc' => $data->status_desc,
+                        ]
+                    );
+                }
+            }
+
             $syncDeleteData->deleteDn();
         } catch (\Throwable $th) {
             $this->syncError(
--- app/Jobs/Syncronization/SyncPurchaseOrderJob.php
+++ app/Jobs/Syncronization/SyncPurchaseOrderJob.php
@@ -2,12 +2,18 @@
 
 namespace App\Jobs\Syncronization;
 
+use Carbon\Carbon;
 use App\Trait\ErrorLog;
+use App\Models\PurchaseOrder\PoDetail;
+use App\Models\PurchaseOrder\PoHeader;
 use Illuminate\Queue\InteractsWithQueue;
+use App\Models\PurchaseOrder\PoDetailErp;
+use App\Models\PurchaseOrder\PoHeaderErp;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Service\Syncronization\SyncDeleteData;
 use App\Service\Syncronization\SyncPurchaseOrderData;
+use Log;
 
 class SyncPurchaseOrderJob implements ShouldQueue
 {
@@ -24,10 +30,95 @@ class SyncPurchaseOrderJob implements ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle(SyncPurchaseOrderData $syncPurchaseOrderData, SyncDeleteData $syncDeleteData): void
+    public function handle(SyncDeleteData $syncDeleteData): void
     {
         try {
-            $syncPurchaseOrderData->syncPurchaseOrder();
+            // Po Header
+            // Initialize year and period
+            $actualYear = Carbon::now()->year;
+            $actualPeriod = Carbon::now()->month;
+            $oneYearsBefore = Carbon::now()->subYears(2)->year; // Change subYears value if you want to sync within range year (Only Running at 00:00 - 00:10)
+            $twoMonthBefore = Carbon::now()->subMonths(2)->month; // Change subMonths value if you want to sync within range month (Running every ten minute)
+
+            if (Carbon::now()->format('h:i') >= '00:00' && Carbon::now()->format('h:i') <= '00:10') {
+                // Get Purchase Order from range two year ago till now
+                $sqlsrvDataPoHeader = PoHeaderErp::whereBetween('po_year', [$oneYearsBefore, $actualYear])
+                    ->where('po_type_desc', 'PO LOCAL')
+                    ->get();
+
+                Log::channel('sync')->info('Sync PO from range two year ago till now');
+            } else {
+                // Get Purchase Order from range two month ago till now on this year
+                $sqlsrvDataPoHeader = PoHeaderErp::whereBetween('po_period', [$twoMonthBefore, $actualPeriod])
+                    ->where('po_year', $actualYear)
+                    ->where('po_type_desc', 'PO LOCAL')
+                    ->get();
+            }
+
+            // copy all data from sql server
+            $poNumber = [];
+            foreach ($sqlsrvDataPoHeader as $data) {
+                $poNumber[] = $data->po_no;
+
+                PoHeader::updateOrCreate(
+                    // find the po_no
+                    [
+                        'po_no' => $data->po_no,
+                        'supplier_code' => $data->supplier_code,
+                    ],
+                    // update data
+                    [
+                        'supplier_name' => $data->supplier_name,
+                        'po_date' => $data->po_date,
+                        'po_year' => $data->po_year,
+                        'po_period' => $data->po_period,
+                        'po_status' => $data->po_status,
+                        'reference_1' => $data->reference_1,
+                        'reference_2' => $data->reference_2,
+                        'attn_name' => $data->attn_name,
+                        'po_currency' => $data->po_currency,
+                        'po_type_desc' => $data->po_type_desc,
+                        'pr_no' => $data->pr_no,
+                        'planned_receipt_date' => $data->planned_receipt_date,
+                        'payment_term' => $data->payment_term,
+                        'po_origin' => $data->po_origin,
+                        'po_revision_no' => $data->po_revision_no,
+                        'po_revision_date' => $data->po_revision_date,
+                    ]
+                );
+            }
+
+            // Po Detail
+            foreach ($poNumber as $data) {
+                $sqlsrvDataPoDetail = PoDetailErp::where('po_no', $data)->get();
+
+                // copy all data from sql server
+                foreach ($sqlsrvDataPoDetail as $data) {
+                    PoDetail::updateOrCreate(
+                        [
+                            'po_no' => $data->po_no,
+                            'po_line' => $data->po_line,
+                        ],
+                        [
+                            'po_sequence' => $data->po_sequence,
+                            'item_code' => $data->item_code,
+                            'code_item_type' => $data->code_item_type,
+                            'bp_part_no' => $data->bp_part_no,
+                            'bp_part_name' => $data->bp_part_name,
+                            'item_desc_a' => $data->item_desc_a,
+                            'item_desc_b' => $data->item_desc_b,
+                            'planned_receipt_date' => $data->planned_receipt_date,
+                            'po_qty' => $data->po_qty,
+                            'receipt_qty' => $data->receipt_qty,
+                            'invoice_qty' => $data->invoice_qty,
+                            'purchase_unit' => $data->purchase_unit,
+                            'price' => $data->price,
+                            'amount' => $data->amount,
+                        ]
+                    );
+                }
+            }
+
             $syncDeleteData->deletePo();
         } catch (\Throwable $th) {
             $this->syncError(
