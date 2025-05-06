0e8aa12e0be6cd59d0f2ba94ddfc5df0b692e406
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-05-06 22:07:44 +0700

    fix: add min_stock_incoming and min_stock_outgoing fields to SubcontAllListItemResource and update data retrieval in SubcontGetListItem

--- app/Service/Subcontractor/SubcontGetListItem.php
+++ app/Service/Subcontractor/SubcontGetListItem.php
@@ -72,8 +72,7 @@ class SubcontGetListItem
         }
 
         // Get all record of user subcont item data
-        $data = SubcontItem::select('sub_item_id', 'item_code', 'item_name', 'item_old_name', 'status')
-            ->where('bp_code', $bp_code)
+        $data = SubcontItem::where('bp_code', $bp_code)
             ->orderBy('item_name', 'asc')
             ->get();
 
